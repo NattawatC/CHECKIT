32c72846c9b7f363a0519bb5daba2d7ecbcdeda4
NattawatC <64011478@kmitl.ac.th>
2023-09-26 22:54:17 +0700

    Create form-handling

---
+++ src/types/user/form.ts
@@ -0,0 +1,17 @@
+import * as z from 'zod'
+
+export const loginFormSchema = z.object({
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+
+  //TODO: uncomment this when we have a backend
+  // .refine(async (e) => {
+  //   // Where checkIfEmailIsValid makes a request to the backend
+  //   // to see if the email is valid.
+  //   return await checkIfEmailIsValid(e);
+  // }, "This email is not in our database")
+  
+  password: z
+    .string()
+    .min(1, 'Password is required')
+    .min(8, 'Password must have than 8 characters'),
+})
