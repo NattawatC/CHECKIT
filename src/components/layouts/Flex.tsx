d6b2d3297dce9e93a9227ad2bd96c0eba5b272a2
NattawatC <64011478@kmitl.ac.th>
2023-09-26 22:50:52 +0700

    Setup layout for mobile

--- src/components/layouts/Flex.tsx
+++ src/components/layouts/Flex.tsx
@@ -1,28 +1,28 @@
 import { cn } from "@/lib/utils";
 import React, { ComponentProps } from "react";
 
-const gapClasses = {
-  sm: "gap-2",
-  base: "gap-4",
-  lg: "gap-6",
-  xl: "gap-12",
-};
+// const gapClasses = {
+//   sm: "gap-2",
+//   base: "gap-4",
+//   lg: "gap-6",
+//   xl: "gap-12",
+// };
 
 interface FlexProps extends ComponentProps<"div"> {
   direction?: "row" | "col";
-  gap?: "sm" | "base" | "lg" | "xl";
+//   gap?: "sm" | "base" | "lg" | "xl";
 }
 
 export const Flex: React.FunctionComponent<FlexProps> = ({
   direction = "row",
-  gap = "base",
+//   gap = "base",
   className,
   children,
 }) => {
   return (
     <div
       className={cn(
-        gapClasses[gap],
+        // gapClasses[gap],
         `flex-${direction}`,
         "flex w-full",
         className,
