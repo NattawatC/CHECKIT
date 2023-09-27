c996fef1bebcf86bce0e06ef683144ec3ac32dd4
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-09-27 21:21:58 +0700

    check mail validation
    can check email patter of user input
    - @gmail.com
    -@yahoo.com
    -@kmitl.ac.th
    -@iclound.com
    -@outlook.com

--- src/services/dataservices.tsx
+++ src/services/dataservices.tsx
@@ -1,20 +1,30 @@
-interface User {
+interface UserLogin {
   email: string
   password: string
 }
 const dataservices = {
-  checkLogin: (user: User) => {
-    let list = [{ email: 'admin@gmail.com', password: 'admin1234' }]
-    if (
-      list.find(
-        (item) => item.email === user.email && item.password === user.password
-      )
-    ) {
-      console.log('login success')
-      console.log(user.email, user.password)
-      return true
+  checkLogin: (user: UserLogin) => {
+    //email format
+    var emailFormat =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const mailPattern =
+      /@(gmail\.com|hotmail\.com|yahoo\.com|kmitl.ac\.th|outlook\.com|icloud.\com)$/i
+    if (emailFormat.test(user.email)) {
+      //check mail format & pattern
+      if (mailPattern.test(user.email)) {
+        //check mail pattern
+        console.log('email format correct')
+        return true
+      } else {
+        //mail pattern incorrect
+        console.log('mail invalid incorrect')
+        return false
+      }
+    } else {
+      //email format incorrect
+      console.log('email format incorrect')
+      return false
     }
   },
 }
-
 export default dataservices
