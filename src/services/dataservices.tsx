3b4cc7d27b9082088c8e7459863d8f05a96baf9c
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-09-27 22:13:20 +0700

    update checkLogin and create checkMailFormat function
    checkMailFormat to check mail format

--- src/services/dataservices.tsx
+++ src/services/dataservices.tsx
@@ -1,21 +1,25 @@
-interface UserLogin {
-  email: string
-  password: string
-}
 const dataservices = {
-  checkLogin: (user: UserLogin) => {
+  checkMailFormat: (email: string) => {
     //email format
     var emailFormat =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     const mailPattern =
       /@(gmail\.com|hotmail\.com|yahoo\.com|kmitl.ac\.th|outlook\.com|icloud.\com)$/i
-    //check mail format & pattern
-    if (emailFormat.test(user.email) && mailPattern.test(user.email)) {
-      console.log('email format correct')
+    if (emailFormat.test(email) && mailPattern.test(email)) {
       return true
     } else {
-      //email format incorrect
-      console.log('email format incorrect')
+      return false
+    }
+  },
+
+  checkLogin: (user: { email: string; password: string }) => {
+    //check email format
+    if (dataservices.checkMailFormat(user.email)) {
+      console.log('Email format is correct')
+      console.log(user)
+      return true
+    } else {
+      console.log('Email format is not correct')
       return false
     }
   },
