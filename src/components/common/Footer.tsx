c8b22915419436bdcc48067a6e76533099d73475
NattawatC <64011478@kmitl.ac.th>
2023-09-27 21:21:29 +0700

    Footer component

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
 const Footer: React.FunctionComponent = () => {
-  return <div>Footer</div>
+  return (
+    <footer className='flex flex-col gap-6 items-center justify-center mx-4 mt-14 mb-4'>
+      <div className="w-[140px] block h-px bg-stone-400">
+      </div>
+      <p className="flex items-center justify-center text-base">All rights reserves @Checkit.co</p>
+    </footer>
+  )
 }
 
 export default Footer
