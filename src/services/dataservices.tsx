4d1f0c2dbd6d03b3d2235d4b99b196e0ad0002e3
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-09-27 23:31:22 +0700

    create check register user func
    edit name var to "_" format

--- src/services/dataservices.tsx
+++ src/services/dataservices.tsx
@@ -2,20 +2,38 @@ const dataservices = {
   //check email format
   checkMailFormat: (email: string) => {
     //mail format
-    var emailFormat =
+    var email_format =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     //mail pattern
-    const mailPattern =
+    const mail_pattern =
       /@(gmail\.com|hotmail\.com|yahoo\.com|kmitl.ac\.th|outlook\.com|icloud.\com)$/i
     //check email format
-    if (emailFormat.test(email) && mailPattern.test(email)) {
+    if (email_format.test(email) && mail_pattern.test(email)) {
       return true
     } else {
       return false
     }
   },
+  //check user register
+  //TODO: connect with database
+  checkRegister: (user: {
+    username: string
+    email: string
+    password: string
+  }) => {
+    //check email format
+    if (dataservices.checkMailFormat(user.email)) {
+      console.log('Email format is correct')
+      return true
+    } else {
+      //email format is not correct
+      console.log('Email format is not correct')
+      return false
+    }
+  },
 
   //check user login
+  //TODO: connect with database
   checkLogin: (user: { email: string; password: string }) => {
     //check email format
     if (dataservices.checkMailFormat(user.email)) {
