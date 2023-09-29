70b241347197ebf4c2585e5026bb4586e9fca22f
jimmy666k <64011397@kmitl.ac.th>
2023-09-30 02:52:28 +0700

    upcoming task component

---
+++ src/components/UpcomingTask.tsx
@@ -0,0 +1,17 @@
+
+
+export const UpcomingTask: React.FunctionComponent = () =>{
+    return(
+        <div className= "w-full h-full bg-custom-white p-2 rounded-lg flex-col justify-center items-center gap-1 inline-flex">
+            <div className="text-custom-black text-base font-medium break-words">
+                Upcoming
+            </div>
+            <div className="text-custom-black text-base font-medium break-words">
+                10 tasks
+            </div>
+        </div>
+
+
+
+    )
+}
