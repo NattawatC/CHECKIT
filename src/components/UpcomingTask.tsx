95521cea3403a55bdd0c50a5c53c30f5efac325e
NattawatC <64011478@kmitl.ac.th>
2023-10-17 00:03:34 +0700

    Change component into button

--- src/components/UpcomingTask.tsx
+++ src/components/UpcomingTask.tsx
@@ -6,14 +6,14 @@ interface UpcomingTaskProps{
 
 export const UpcomingTask: React.FunctionComponent<UpcomingTaskProps> = ({taskNum}) =>{
     return(
-        <div className= "w-full h-full bg-custom-white p-2 rounded-lg flex-col justify-center items-center gap-1 inline-flex">
+        <button className= "w-full h-full bg-custom-white p-2 rounded-lg flex-col justify-center items-center gap-1 inline-flex">
             <div className="text-custom-black text-base font-medium break-words">
                 Upcoming
             </div>
             <div className="text-custom-black text-base font-medium break-words">
                 {taskNum} Task
             </div>
-        </div>
+        </button>
 
 
 
