1748dfc6afe8318e527dca44228a2d79de3d9f82
jimmy666k <64011397@kmitl.ac.th>
2023-10-17 00:25:55 +0700

    update personal page(unfinished)

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { NavBar, SearchBar } from "@/components/common"
 import { MainLayout } from "@/components/layouts"
-import { NextPage } from "next"
 import dataservices from "@/services/dataservices"
+import { NextPage } from "next"
 
 const data = new dataservices()
 const taskNum = data.getUserInfo()
-
+const personal_task = data.getAllTaskByCategory("Personal")
+console.log(personal_task)
 
 const Personal: NextPage = () => {
     return(
+        <>
         <div className="bg-custom-black">
             <MainLayout>
                 <div className="flex flex-col gap-4">
@@ -19,12 +21,12 @@ const Personal: NextPage = () => {
                     </div>
                     <div className="gap-1 items-center flex flex-col text-custom-white font-medium">
                         <p className="text-xl">Personal</p>
-                        <p className="text-base">{}</p>
+                        {/* <p className="text-base">{personal_task} Task</p> */}
                     </div>
                 </div>
             </MainLayout>
-
         </div>
+        </>
     )
 }
 export default Personal    
