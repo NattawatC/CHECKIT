817951dc1b10190aff1c70f65e8ed128b30055c1
NattawatC <64011478@kmitl.ac.th>
2023-10-16 23:29:17 +0700

    Update Login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { NavBar, Footer } from '@/components/common'
+import { Footer } from '@/components/common'
 import { MainLayout } from '@/components/layouts'
 import { Button } from '@/components/ui/button'
 import {
@@ -11,7 +11,6 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import dataservices from '@/services/dataservices'
 import { loginFormSchema } from '@/types/user/form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { NextPage } from 'next'
@@ -71,12 +70,18 @@ export function LoginForm() {
         </div>
 
         <p className="text-right text-sm text-gray-600 mt-2">
-          <Link className="hover:underline hover:text-custom-orangeHover" href="">
+          <Link
+            className="hover:underline hover:text-custom-orangeHover"
+            href=""
+          >
             Forgot password?{' '}
           </Link>
         </p>
 
-        <Button className="py-3 px-4 w-full mt-6 text-[18px] inline-flex" type="submit">
+        <Button
+          className="py-3 px-4 w-full mt-6 text-[18px] inline-flex"
+          type="submit"
+        >
           Login
         </Button>
       </form>
@@ -90,9 +95,7 @@ const Login: NextPage = () => {
       <MainLayout className="flex flex-col gap-16">
         <div className="flex flex-col items-center justify-center">
           <p className="font-semibold text-[32px]">Welcome Back!</p>
-          <p className="font-normal text-base">
-            Please enter your details
-          </p>
+          <p className="font-normal text-base">Please enter your details</p>
         </div>
         <LoginForm />
         <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
@@ -102,7 +105,12 @@ const Login: NextPage = () => {
 
         <p className="text-center text-sm text-gray-600">
           Don&apos;t have an account?{' '}
-          <Link href={'/register'} className='hover:underline text-custom-orange'>Sign up</Link>
+          <Link
+            href={'/register'}
+            className="hover:underline text-custom-orange"
+          >
+            Sign up
+          </Link>
         </p>
       </MainLayout>
       {/* <Image
@@ -110,7 +118,7 @@ const Login: NextPage = () => {
         sizes="(max-width: 100%), (max-width: 100%)"
         alt="Login Footer Pic"
       /> */}
-      <Footer className='text-custom-black'/>
+      <Footer className="text-custom-black" />
     </>
   )
 }
