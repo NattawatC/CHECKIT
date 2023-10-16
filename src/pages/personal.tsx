ae40ee9fce6526be0893a668a64364d2317ed5da
jimmy666k <64011397@kmitl.ac.th>
2023-10-17 00:06:50 +0700

    create personal page

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -1,11 +1,27 @@
+'use client'
+import { NavBar, SearchBar } from "@/components/common"
 import { MainLayout } from "@/components/layouts"
 import { NextPage } from "next"
+import dataservices from "@/services/dataservices"
+
+const data = new dataservices()
+const taskNum = data.getUserInfo()
+
 
 const Personal: NextPage = () => {
     return(
         <div className="bg-custom-black">
             <MainLayout>
-
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-8">
+                        <NavBar />
+                        <SearchBar />
+                    </div>
+                    <div className="gap-1 items-center flex flex-col text-custom-white font-medium">
+                        <p className="text-xl">Personal</p>
+                        <p className="text-base">{}</p>
+                    </div>
+                </div>
             </MainLayout>
 
         </div>
