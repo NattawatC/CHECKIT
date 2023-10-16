b03928356b8bbddd2056163c7576e15ec8ed86ab
jimmy666k <64011397@kmitl.ac.th>
2023-10-16 23:41:50 +0700

    Create personal.tsx

---
+++ src/pages/personal.tsx
@@ -0,0 +1,14 @@
+import { MainLayout } from "@/components/layouts"
+import { NextPage } from "next"
+
+const Personal: NextPage = () => {
+    return(
+        <div className="bg-custom-black">
+            <MainLayout>
+
+            </MainLayout>
+
+        </div>
+    )
+}
+export default Personal    
