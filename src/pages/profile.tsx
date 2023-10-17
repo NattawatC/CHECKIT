a4f811696bfefc4573bb5a556783aab953773e36
jimmy666k <64011397@kmitl.ac.th>
2023-10-17 23:51:31 +0700

    update profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,38 @@
-import { NavBar } from '@/components/common'
-import React from 'react'
+import { Footer, NavBar } from '@/components/common'
+import { MainLayout } from '@/components/layouts'
+import { Button } from '@/components/ui/button'
+import dataservices from '@/services/dataservices'
+
+const data = new dataservices()
+
+
 
 const Profile = () => {
   return (
-    <NavBar/>
+    <>
+    <div className="bg-custom-black">
+      <MainLayout>
+        <div className="flex flex-col gap-8">
+          <NavBar />
+          <div className="flex flex-col items-start font-medium text-custom-white">
+            <p className="text-xs">{data.getUserInfo().date}</p>
+            <p className="text-xl">See your Profile</p>
+          </div>
+
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-row gap-5 text-base text-custom-white">
+              <p>Name:</p>
+              <p>{data.getUserInfo().username}</p>
+            </div>
+            <Button className="px-2 py-1 text-xs h-fit bg-custom-gray">Edit</Button>
+            </div>
+            
+        </div>
+      </MainLayout>
+      <Footer className="text-custom-white"/>
+    </div>
+    </>
+    
   )
 }
 
