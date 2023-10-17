f7624fb21178e0fc6a46ba0953117467a426531f
jimmy666k <64011397@kmitl.ac.th>
2023-10-18 00:22:08 +0700

    update profile page (hover not done yet)

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,6 +2,7 @@ import { Footer, NavBar } from '@/components/common'
 import { MainLayout } from '@/components/layouts'
 import { Button } from '@/components/ui/button'
 import dataservices from '@/services/dataservices'
+import { IoIosInformationCircle } from 'react-icons/io'
 
 const data = new dataservices()
 
@@ -25,7 +26,26 @@ const Profile = () => {
               <p>{data.getUserInfo().username}</p>
             </div>
             <Button className="px-2 py-1 text-xs h-fit bg-custom-gray">Edit</Button>
+          </div>
+
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-row gap-5 text-base text-custom-white">
+              <p>Email:</p>
+              <p>{data.getUserInfo().email}</p>
             </div>
+            <Button className="px-2 py-1 text-xs h-fit bg-custom-gray">Edit</Button>
+          </div>
+
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-row gap-3 items-center">
+              <p className="text-custom-white text-base font-medium">Done</p>
+              <p className="text-custom-white"><IoIosInformationCircle /></p>
+            </div>
+            <div className="flex flex-col rounded-lg gap-2 bg-custom-gray">
+              <p>deleted task</p>
+              <p>deleted task</p>
+            </div>
+          </div>
             
         </div>
       </MainLayout>
