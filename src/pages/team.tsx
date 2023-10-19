cd32124c5be9129c763ad6c08e3dcdea62d55180
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2023-10-20 00:45:25 +0700

    create AddTeamModal

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -5,6 +5,33 @@ import { Button } from '@/components/ui/button'
 import dataservices from '@/services/dataservices'
 import { NextPage } from 'next'
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import MemberItem from '@/components/teamPage/MemberItem'
+
+//hard code the email example (MemberItem)
+const features = [
+  {
+    email: 'Easy to use for people who are asdasdaas',
+  },
+  {
+    email: 'Trusted App',
+  },
+  {
+    email: 'Sharing',
+  },
+  {
+    email: '100% Free',
+  },
+]
+
 const data = new dataservices()
 const team_info = data.getAllTeamOfUser()
 
@@ -28,9 +55,52 @@ const Team: NextPage = () => {
               />
             ))}
           </div>
-          <Button className="w-full py-2 px-4 bg-custom-orange hover:bg-custom-orangeHover text-base inline-flex">
-            Create
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                variant="outline"
+                className="rounded-lg text-custom-white border-custom-orange w-full py-2 px-4 bg-custom-orange hover:bg-custom-orangeHover text-base inline-flex"
+              >
+                Create
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px] flex flex-col gap-8 mx-auto w-full max-w-md px-4 py-14 md:py-24 bg-custom-black ">
+              <DialogHeader>
+                <DialogTitle className="text-custom-white text-left">
+                  Collaborate with Friends
+                </DialogTitle>
+              </DialogHeader>
+              <div className="flex flex-col gap-5">
+                <label className="text-custom-white">Team Name</label>
+                <input
+                  className="bg-custom-black p-2 w-full h-auto text-custom-white outline-none border-b"
+                  type="text"
+                  placeholder="Placeholder text"
+                />
+              </div>
+              <div className="flex flex-col gap-4 w-full">
+                <label className="text-custom-white">Email</label>
+                <div className="flex flex-row gap-5">
+                  <input
+                    className="bg-custom-black p-2 w-full h-auto text-custom-white outline-none border-b"
+                    type="text"
+                    placeholder="abc@gmail.com"
+                  />
+                  <Button className="rounded-lg text-custom-white border-custom-orange py-1 px-4 bg-custom-orange inline-flex">
+                    Add
+                  </Button>
+                </div>
+              </div>
+              <div className="flex flex-wrap gap-2">
+                {features.map((feature) => (
+                  <MemberItem key={feature.email} email={feature.email} />
+                ))}
+              </div>
+              <Button className="rounded-lg text-custom-white w-full bg-custom-gray inline-flex">
+                Finish Construct
+              </Button>
+            </DialogContent>
+          </Dialog>
         </div>
       </MainLayout>
       <Footer className="text-custom-white" />
