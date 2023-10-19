ca95916e23d43ab04167b30f55c1a599bb2877fd
NattawatC <64011478@kmitl.ac.th>
2023-10-20 00:30:10 +0700

    Fix footer layout

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,11 +4,13 @@ interface FooterProps {
 
 export const Footer: React.FunctionComponent<FooterProps> = ({ className }) => {
   return (
-    <footer className="flex flex-col gap-6 items-center justify-center px-4 pt-14 pb-4">
-      <div className={`w-[140px] block h-px bg-neutral-500`}></div>
-      <p className={`flex items-center justify-center text-base ${className}`}>
-        All rights reserves @Checkit.co
-      </p>
-    </footer>
+      <footer className="flex flex-col gap-6 items-center justify-center mt-auto py-4">
+        <div className={`w-[140px] block h-px bg-neutral-500`}></div>
+        <p
+          className={`flex items-center justify-center text-base ${className}`}
+        >
+          All rights reserves @Checkit.co
+        </p>
+      </footer>
   )
 }
