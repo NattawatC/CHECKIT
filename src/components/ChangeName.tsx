db61b35186da2e7b7a7768cf2c2d2b8c12518707
jimmy666k <64011397@kmitl.ac.th>
2023-10-19 23:04:30 +0700

    remove comment

--- src/components/ChangeName.tsx
+++ src/components/ChangeName.tsx
@@ -40,20 +40,3 @@ export default DialogChangeName
 
 
 
-// 'use client'
-// import React from "react";
-
-
-// const ChangeName: React.FunctionComponent = () => {
-//     return(
-//         <div className="flex flex-col gap-3 rounded-lg justify-center bg-custom-gray px-2 py-2">
-//             <p className="text-custom-white text-base font-medium">Change your name</p>
-//             <div className="flex flex-row gap-5 relative">
-//                 <input type="text" className="bg-custom-gray text-custom-white outline-none border-b border-custom-white w-full" />
-//                 <button className="rounded-lg px-2 bg-custom-orange text-custom-white font-medium text-xs">Save</button>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default ChangeName
