f4b00444c72c8172aca4a4b589b0aaa3d7f8c359
jimmy666k <64011397@kmitl.ac.th>
2023-10-20 00:01:09 +0700

    update edit button

--- src/components/ChangeEmail.tsx
+++ src/components/ChangeEmail.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Button } from "@/components/ui/button"
+import { useState } from 'react';
 import {
   Dialog,
   DialogContent,
@@ -12,18 +13,29 @@ import {
 
 
 export function DialogChangeEmail() {
+  const [email,setEmail] = useState('');
+
+  const handleSaveClick = () => {
+    console.log('Email:', email);
+  
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className="px-2 py-1 text-xs h-fit bg-custom-gray">Edit</Button>
+        <Button className="px-4 py-1 text-xs h-fit bg-custom-gray">Edit</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px] bg-custom-gray rounded-lg">
         <DialogHeader>
-          <DialogTitle className="flex text-custom-white items-start">Change your email</DialogTitle>
+          <DialogTitle className="flex text-custom-white items-start">Change your Email</DialogTitle>
         </DialogHeader>
         <div className="flex flex-row gap-5 relative">
-                <input type="email" className="bg-custom-gray text-custom-white outline-none border-b border-custom-white w-full" />
-                <button className="rounded-lg px-2 bg-custom-orange text-custom-white font-medium text-xs">Save</button>
+                <input type="email" value={email} onChange={handleInputChange} className="bg-custom-gray text-custom-white outline-none border-b border-custom-white w-full" />
+                <button onClick={handleSaveClick} className="rounded-lg px-2 bg-custom-orange text-custom-white font-medium text-xs">Save</button>
         </div>
     
       </DialogContent>
--- src/components/ChangeName.tsx
+++ src/components/ChangeName.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Button } from "@/components/ui/button"
+import { useState } from 'react';
 import {
   Dialog,
   DialogContent,
@@ -12,18 +13,27 @@ import {
 
 
 export function DialogChangeName() {
+  const [name,setName] = useState('');
+
+  const handleSaveClick = () => {
+    console.log('Name:', name);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className="px-2 py-1 text-xs h-fit bg-custom-gray">Edit</Button>
+        <Button className="px-4 py-1 text-xs h-fit bg-custom-gray">Edit</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px] bg-custom-gray rounded-lg">
         <DialogHeader>
           <DialogTitle className="flex text-custom-white items-start">Change your name</DialogTitle>
         </DialogHeader>
         <div className="flex flex-row gap-5 relative">
-                <input type="text" className="bg-custom-gray text-custom-white outline-none border-b border-custom-white w-full" />
-                <button className="rounded-lg px-2 bg-custom-orange text-custom-white font-medium text-xs">Save</button>
+                <input type="text" value={name} onChange={handleInputChange} className="bg-custom-gray text-custom-white outline-none border-b border-custom-white w-full" />
+                <button onClick={handleSaveClick} className="rounded-lg px-2 bg-custom-orange text-custom-white font-medium text-xs">Save</button>
         </div>
     
       </DialogContent>
