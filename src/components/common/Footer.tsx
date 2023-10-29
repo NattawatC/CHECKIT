b4ebc060fd954e2c0f7e2895fb0fd886114977be
NattawatC <64011478@kmitl.ac.th>
2023-10-30 00:58:26 +0700

    Fix footer layout

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,11 +1,12 @@
 interface FooterProps {
   className: string
+  isHidden?: boolean
 }
 
-export const Footer: React.FunctionComponent<FooterProps> = ({ className }) => {
+export const Footer: React.FunctionComponent<FooterProps> = ({ className, isHidden }) => {
   return (
-      <footer className="flex flex-col gap-6 items-center justify-center mt-auto py-4">
-        <div className={`w-[140px] block h-px bg-neutral-500`}></div>
+      <footer className={`flex flex-col gap-6 items-center justify-center mt-auto py-4 ${isHidden ? 'lg:hidden' : ''}`}>
+        <div className={`w-[140px] block h-px bg-neutral-500 ${className}`}></div>
         <p
           className={`flex items-center justify-center text-base ${className}`}
         >
