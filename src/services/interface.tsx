8bbef99d448eb9d1108977e901852ba8e5968c09
chananon-n <91945879+chananon-n@users.noreply.github.com>
2023-11-09 04:58:24 +0700

    change from class to function

---
+++ src/services/interface.tsx
@@ -0,0 +1,13 @@
+interface Task {
+  id: string
+  title: string
+  note: string
+  date_start: string
+  date_end: string
+  time_start: string
+  time_end: string
+  priority: string
+  role: string[]
+  category: string
+  status: boolean
+}
