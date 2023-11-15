d82f7fde87bd135265cce6c22f5f44040f8e479a
NattawatC <64011478@kmitl.ac.th>
2023-11-16 00:04:11 +0700

    Responsive categoryItem

--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -16,20 +16,20 @@ const CategoryItem: React.FunctionComponent<CategoryProps> = ({
 }) => {
   return (
     <button>
-          <Link href={`${href}`}>
-          <div className="bg-custom-gray rounded-t-lg text-custom-white px-3 py-2">
-            {Icon && <Icon className="h-10 w-9" />}
+      <Link href={`${href}`}>
+        <div className="bg-custom-gray rounded-t-lg text-custom-white px-3 py-2">
+          {Icon && <Icon className="h-10 w-9" />}
+        </div>
+        <div className="flex flex-col bg-custom-white rounded-b-lg p-3 gap-6">
+          <div className="text-left">
+            <p className="text-custom-black text-xl lg:text-2xl">{title}</p>
           </div>
-          <div className="flex flex-col bg-custom-white rounded-b-lg p-3 gap-6">
-            <div className="text-left">
-              <p className="text-custom-black text-xl">{title}</p>
-            </div>
-            <div className="text-right">
-              <p className="text-custom-black text-xl">{taskNum}</p>
-            </div>
+          <div className="text-right">
+            <p className="text-custom-black text-xl lg:text-2xl">{taskNum}</p>
           </div>
-        </Link>
-        </button>
+        </div>
+      </Link>
+    </button>
   )
 }
 
