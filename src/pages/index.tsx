ac781fa2f8bdb72f0ad12eb8f505c4382d58e82a
NattawatC <64011478@kmitl.ac.th>
2023-11-16 00:06:38 +0700

    Responsive home page + icon; needs description tho

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import howitworkPic from '@/assets/howitworkPic.jpg'
 import { Hamburger } from '@/components/common'
 import { Footer } from '@/components/common/Footer'
 import Card from '@/components/home/Card'
@@ -5,24 +6,31 @@ import { MainLayout } from '@/components/layouts'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
+import { BsArrowRightShort, BsFileEarmarkPersonFill } from 'react-icons/bs'
 import { FaGithubSquare } from 'react-icons/fa'
-import { FaSquareEnvelope } from 'react-icons/fa6'
+import { FaHandHoldingHeart, FaSquareEnvelope } from 'react-icons/fa6'
+import { GoShare } from 'react-icons/go'
+import { LuBadgePercent } from 'react-icons/lu'
 import logoMobile from 'src/assets/logoMobile.png'
 
 const features = [
   {
+    icon: BsFileEarmarkPersonFill,
     title: 'User Friendly',
     description: 'Easy to use for people who are either new or professional',
   },
   {
+    icon: FaHandHoldingHeart,
     title: 'Trusted App',
     description: 'Official application that is trusted for security',
   },
   {
+    icon: GoShare,
     title: 'Sharing',
     description: 'Share your task between your besties',
   },
   {
+    icon: LuBadgePercent,
     title: '100% Free',
     description: 'This application is completely free, without any charges',
   },
@@ -31,62 +39,110 @@ const features = [
 export default function Home() {
   return (
     <>
-      <div className="px-4 py-2 float-right">
+      <div className="px-4 py-2 float-right lg:hidden">
         <Hamburger className="text-custom-white" />
       </div>
-      <MainLayout className="bg-custom-black">
-        <div className=" flex flex-col items-center gap-9">
-          <Image src={logoMobile} alt="logo mobile" />
-          <p className="text-custom-white text-center">
-            Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
-            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
-            feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra,
-          </p>
-          <Link href="/login">
-            <Button className="bg-custom-orange text-custom-white">
-              Get Started
-            </Button>
-          </Link>
-        </div>
-      </MainLayout>
+      <div className="flex p-5 float-right">
+        <Link href="/login">
+          <button className="hidden lg:block rounded-md py-1 px-5 bg-custom-white text-custom-black hover:bg-custom-orange hover:text-custom-white ">
+            Login
+          </button>
+        </Link>
+      </div>
+      <div className="bg-custom-black">
+        <MainLayout>
+          <div className=" flex flex-col lg:justify-center gap-9 lg:gap-14">
+            <Image
+              src={logoMobile}
+              width="0"
+              height="0"
+              sizes="100vw"
+              className="w-full h-auto"
+              alt="logo mobile"
+            />
+            <p className="text-custom-white text-center text-base lg:text-xl">
+              Gorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra,
+            </p>
+            <Link href="/register">
+              <Button className="py-3 px-4 w-full inline-flex bg-custom-orange text-custom-white text-base rounded-md lg:text-2xl hover:bg-custom-orangeHover">
+                Get Started <BsArrowRightShort size={30} />
+              </Button>
+            </Link>
+          </div>
+        </MainLayout>
+      </div>
 
-      <section className="py-9 flex flex-col gap-9 px-6">
+      <section className="flex flex-col px-6 py-9 gap-9 lg:py-32">
         <div className="flex flex-col items-center gap-3">
-          <p className="text-center text-base">- Features -</p>
-          <p className="text-center text-2xl">Our Special Features</p>
+          <p className="text-center text-base lg:text-2xl">- Features -</p>
+          <p className="text-center text-2xl lg:text-3xl">
+            Our Special Features
+          </p>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 lg:flex-row lg:justify-center lg:gap-16">
           {features.map((feature) => (
             <Card
               key={feature.title}
               title={feature.title}
               description={feature.description}
+              Icon={feature.icon}
             />
           ))}
         </div>
       </section>
 
-      <MainLayout className=" bg-custom-black">
-        <div className=" flex flex-col items-center gap-9">
-          <p className="text-custom-white text-center text-xl">How it works?</p>
-          <p className="text-custom-white text-center text-base">
-            Qorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
-            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
-            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
-            feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra.
-          </p>
+      <div className="flex flex-row bg-black items-center justify-between">
+        <div className="relative hidden lg:block">
+          <Image
+            src={howitworkPic}
+            width={500}
+            objectFit="contain"
+            alt="logo mobile"
+          />
         </div>
-      </MainLayout>
+        {/* Mobile Phone */}
+        <MainLayout className="flex lg:hidden">
+          <div className="flex flex-col gap-9 items-center lg:justify-center">
+            <p className="flex text-custom-white text-xl lg:text-4xl">
+              How does it work?
+            </p>
+            <p className="text-custom-white text-center text-base lg:text-justify lg:text-xl">
+              Qorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra.
+            </p>
+          </div>
+        </MainLayout>
+        {/* Website */}
+        <div className="hidden lg:block px-20 py-14 mx-0 w-full">
+          <div className="flex flex-col gap-9 justify-center">
+            <p className="flex text-custom-white lg:text-4xl">
+              How does it work?
+            </p>
+            <p className="text-custom-white lg:text-justify lg:text-xl">
+              Qorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus. Class aptent taciti sociosqu ad litora torquent
+              per conubia nostra.
+            </p>
+          </div>
+        </div>
+      </div>
 
       <MainLayout>
         <div className="flex flex-col items-center gap-6">
-          <p className="text-xl text-center">CHECKIT</p>
-          <p className="text-base text-center flex flex-row">
+          <p className="text-xl text-bold lg:text-2xl">CHECKIT</p>
+          <p className="text-base text-center flex flex-row lg:text-xl">
             Use the shortest possible time to create tasks, use all the free
             features to make the most of them.
           </p>
