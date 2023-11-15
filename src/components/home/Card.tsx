ea0d863a857d14af5a3ddb80716c4b5f2561de38
NattawatC <64011478@kmitl.ac.th>
2023-11-16 00:05:24 +0700

    Responsive Card in home page

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -1,21 +1,34 @@
 import React from 'react'
 
-interface CardProps 
-{
-    title: string
-    description: string
+interface CardProps {
+  title: string
+  description: string
+  Icon: React.ElementType
 }
 
-const Card : React.FunctionComponent<CardProps> = ({title, description}) => {
+const Card: React.FunctionComponent<CardProps> = ({
+  title,
+  description,
+  Icon,
+}) => {
   return (
-    <div className='flex flex-col p-4 gap-2 bg-custom-black rounded-2xl '>
-        <p className='text-custom-white text-center  text-xl'>
-            {title}
-        </p>
-        <p className='text-custom-white text-center text-base'>
+    <>
+      {/* Mobile */}
+      <div className="flex flex-col p-4 gap-2 bg-custom-black rounded-2xl lg:hidden">
+        <p className="text-custom-white text-center text-xl">{title}</p>
+        <p className="text-custom-white text-center text-base">{description}</p>
+      </div>
+      {/* Desktop */}
+      <div className="hidden lg:block">
+        <div className="flex flex-col items-center py-8 px-4 gap-2 bg-custom-black rounded-2xl w-64 h-66">
+          {Icon && <Icon className="text-custom-white" size={50} />}
+          <p className="text-custom-white text-center text-2xl">{title}</p>
+          <p className="text-custom-white text-center text-base">
             {description}
-        </p>
-    </div>
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
