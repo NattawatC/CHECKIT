fbbf1745792b44b48007c7007d88f69a9c12f72b
NattawatC <64011478@kmitl.ac.th>
2023-11-16 00:07:24 +0700

    Responsive dashboard + try functions

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,59 +3,68 @@ import CategoryItem from '@/components/CategoryItem'
 import { UpcomingTask } from '@/components/UpcomingTask'
 import { Footer, NavBar } from '@/components/common'
 import { MainLayout } from '@/components/layouts'
-import dataservices from '@/services/dataservices'
+import { getUserInfo } from '@/services/userServices'
 import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 import { BsHeartPulse, BsPerson } from 'react-icons/bs'
 import { PiDotsThreeCircleLight, PiSuitcaseSimpleLight } from 'react-icons/pi'
+import logoMobile from 'src/assets/logoMobile.png'
 
-// Fetch user information and task data
-const data = new dataservices()
-const taskNum = await data.getUserInfo()
+//Initialize taskNum
+const userInfo = await getUserInfo()
 
 // Define categories with taskNum properties
 const categories = [
   {
+    index: 0,
     icon: BsPerson,
     title: 'Personal',
-    taskNum: taskNum.personal_task.length,
+    taskNum: userInfo.personal_task.length,
     href: '/personal',
   },
   {
+    index: 1,
     icon: PiSuitcaseSimpleLight,
     title: 'Work',
-    taskNum: taskNum.work_task.length,
-    href: '/works',
+    taskNum: userInfo.work_task.length,
+    href: '/work',
   },
   {
+    index: 2,
     icon: BsHeartPulse,
     title: 'Health',
-    taskNum: taskNum.health_task.length,
+    taskNum: userInfo.health_task.length,
     href: '/health',
   },
   {
+    index: 3,
     icon: PiDotsThreeCircleLight,
     title: 'Others',
-    taskNum: taskNum.others_task.length,
+    taskNum: userInfo.others_task.length,
     href: '/others',
   },
 ]
+
 const Dashboard: NextPage = () => {
   return (
     <div className="bg-custom-black">
-      <MainLayout>
+      <MainLayout className="lg:hidden">
         <div className="flex-col flex gap-8">
           <NavBar />
-
           <div className="flex flex-col gap-1 text-custom-white font-medium">
-            <p className="text-xs">{taskNum.date}</p>
-            <p className="text-xl">Welcome, {taskNum.username}</p>
+            <p className="text-xs">{userInfo.date}</p>
+            <p className="text-xl">Welcome, {userInfo.username}</p>
           </div>
-          <UpcomingTask taskNum={taskNum.upcoming_task.length} />
-
-          <div className="grid grid-cols-2 gap-4">
-            {categories.map((category, index) => (
+          <UpcomingTask
+            taskNum={userInfo.upcoming_task.length}
+            href="/upcoming"
+          />
+          {/* Mobile */}
+          <div className="grid grid-cols-2 gap-4 lg:hidden">
+            {categories.map((category) => (
               <CategoryItem
-                key={index}
+                key={category.index}
                 Icon={category.icon}
                 title={category.title}
                 taskNum={category.taskNum}
@@ -65,6 +74,45 @@ const Dashboard: NextPage = () => {
           </div>
         </div>
       </MainLayout>
+      {/* Desktop */}
+      <div className="hidden lg:block min-h-screen">
+        <div className="flex flex-row gap-10 py-10">
+          <div className="flex flex-col">
+            <Link href="/register">
+              <Image
+                src={logoMobile}
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="w-full h-auto"
+                alt="logo mobile"
+              />
+              <NavBar />
+            </Link>
+          </div>
+          <div className="flex flex-col gap-8 w-full px-10">
+            <div className="flex flex-col gap-2 text-custom-white font-medium">
+              <p className="text-xl">{userInfo.date}</p>
+              <p className="text-3xl">Welcome, {userInfo.username}</p>
+            </div>
+            <UpcomingTask
+              taskNum={userInfo.upcoming_task.length}
+              href="/upcoming"
+            />
+            <div className="grid grid-cols-4 gap-4 max-w-full">
+              {categories.map((category) => (
+                <CategoryItem
+                  key={category.index}
+                  Icon={category.icon}
+                  title={category.title}
+                  taskNum={category.taskNum}
+                  href={category.href}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer className="text-custom-white" />
     </div>
   )
