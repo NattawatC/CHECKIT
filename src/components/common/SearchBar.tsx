85c78eaef31f5f7dc96aa746d593d1757f2434b1
NattawatC <64011478@kmitl.ac.th>
2023-11-16 00:09:57 +0700

    Responsive

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -12,13 +12,13 @@ export const SearchBar: React.FC = () => {
     setIsFilter(!isFilter)
   }
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 px-20">
       <div className="relative w-full">
         <AiOutlineSearch className="absolute top-0 bottom-0 w-6 h-6 my-auto text-custom-white left-3" />
         <Input
           type="text"
           placeholder="Search"
-          className="w-full pl-12 pr-12 rounded-lg bg-custom-gray text-custom-white border-custom-white focus-visible:ring-custom-orange focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
+          className="w-full pl-12 pr-12 rounded-lg bg-custom-gray text-custom-white border-custom-white focus-visible:ring-custom-orange focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none lg:text-lg"
         />
         <Button
           className="absolute top-0 bottom-0 my-auto bg-transparent right-0 focus:bg-transparent"
