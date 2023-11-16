4c7d12493ac83cf45091557e1e09bd94fd1f32fd
NattawatC <64011478@kmitl.ac.th>
2023-11-16 19:52:13 +0700

    Refactor UpcomingTask and Others components

--- src/components/UpcomingTask.tsx
+++ src/components/UpcomingTask.tsx
@@ -1,21 +1,24 @@
-import dataservices from "@/services/dataservices"
+import Link from 'next/link'
 
-interface UpcomingTaskProps{
-    taskNum: number
+interface UpcomingTaskProps {
+  taskNum: number
+  href: string
 }
 
-export const UpcomingTask: React.FunctionComponent<UpcomingTaskProps> = ({taskNum}) =>{
-    return(
-        <button className= "w-full h-full bg-custom-white p-2 rounded-lg flex-col justify-center items-center gap-1 inline-flex">
-            <div className="text-custom-black text-base font-medium break-words">
-                Upcoming
-            </div>
-            <div className="text-custom-black text-base font-medium break-words">
-                {taskNum} Task
-            </div>
-        </button>
-
-
-
-    )
+export const UpcomingTask: React.FunctionComponent<UpcomingTaskProps> = ({
+  taskNum,
+  href,
+}) => {
+  return (
+    <button className="w-full h-full bg-custom-white p-2 rounded-lg flex-col justify-center items-center gap-1 inline-flex">
+      <Link href={`${href}`}>
+        <div className="text-custom-black text-base font-medium break-words lg:text-2xl">
+          Upcoming
+        </div>
+        <div className="text-custom-black text-base font-medium break-words lg:text-2xl">
+          {taskNum} Tasks
+        </div>
+      </Link>
+    </button>
+  )
 }
