4ec22c3e64127092abee6db3ace8dffb84d8e716
NattawatC <64011478@kmitl.ac.th>
2023-11-16 15:27:20 +0700

    Responsive Health page

--- src/pages/health.tsx
+++ src/pages/health.tsx
@@ -1,77 +1,75 @@
 'use client'
-import { Footer, NavBar, SearchBar } from "@/components/common"
-import { MainLayout } from "@/components/layouts"
-import TaskItem from "@/components/taskPage/TaskItem"
-import { filterByCategory } from "@/services/userServices"
-import { NextPage } from "next"
+import { Footer, NavBar, SearchBar } from '@/components/common'
+import { MainLayout } from '@/components/layouts'
+import TaskItem from '@/components/taskPage/TaskItem'
+import { filterByCategory } from '@/services/userServices'
+import { NextPage } from 'next'
 
 const health_task = await filterByCategory('Health')
 
 const Health: NextPage = () => {
-    return (
-        <>
-            <div className="bg-custom-black min-h-screen">
-                <MainLayout className="lg:hidden">
-                    <div className="flex flex-col gap-4">
-                        <div className="flex flex-col gap-8">
-                            <NavBar />
-                            <SearchBar />
-                        </div>
-                        <div className=" gap-1 items-center flex flex-col text-custom-white font-medium">
-                            <p className="text-xl">Health</p>
-                            <p className="text-base">{health_task.length} Tasks</p>
-                        </div>
-
-                        <div className="flex flex-col gap-4 bg-custom-white rounded-lg p-4">
-
-                            {health_task.map((item, index) => (
-                                <TaskItem
-                                    key={index}
-                                    priority={item.priority}
-                                    title={item.title}
-                                    date_start={item.date_start}
-                                    date_end={item.date_end}
-                                    note={item.note}
-                                    time_start={item.time_start}
-                                    time_end={item.time_end}
-                                />
-                            ))}
-
-                        </div>
-                    </div>
-                </MainLayout>
-
-                {/* Desktop */}
-                <div className="hidden lg:block min-h-screen">
-                    <div className="flex flex-row gap-10 py-10">
-                        <NavBar />
-                        <div className="flex flex-col w-full px-52 gap-8">
-                            <SearchBar />
-                            <div className="gap-1 items-center flex flex-col text-custom-white font-medium">
-                                <p className="text-2xl">Health</p>
-                                <p className="text-xl">{health_task.length} Tasks</p>
-                            </div>
-                            <div className="flex flex-col gap-4 bg-custom-white rounded-lg p-4">
-                                {health_task.map((item, index) => (
-                                    <TaskItem
-                                        key={index}
-                                        priority={item.priority}
-                                        title={item.title}
-                                        date_start={item.date_start}
-                                        date_end={item.date_end}
-                                        note={item.note}
-                                        time_start={item.time_start}
-                                        time_end={item.time_end}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Footer className="text-custom-white" />
+  return (
+    <>
+      <div className="bg-custom-black min-h-screen">
+        <MainLayout className="lg:hidden">
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-8">
+              <NavBar />
+              <SearchBar />
             </div>
-        </>
-    )
+            <div className=" gap-1 items-center flex flex-col text-custom-white font-medium">
+              <p className="text-xl">Health</p>
+              <p className="text-base">{health_task.length} Tasks</p>
+            </div>
+
+            <div className="flex flex-col gap-4 bg-custom-white rounded-lg p-4">
+              {health_task.map((item, index) => (
+                <TaskItem
+                  key={index}
+                  priority={item.priority}
+                  title={item.title}
+                  date_start={item.date_start}
+                  date_end={item.date_end}
+                  note={item.note}
+                  time_start={item.time_start}
+                  time_end={item.time_end}
+                />
+              ))}
+            </div>
+          </div>
+        </MainLayout>
+
+        {/* Desktop */}
+        <div className="hidden lg:block min-h-screen">
+          <div className="flex flex-row gap-10 py-10">
+            <NavBar />
+            <div className="flex flex-col w-full px-52 gap-8">
+              <SearchBar />
+              <div className="gap-1 items-center flex flex-col text-custom-white font-medium">
+                <p className="text-2xl">Health</p>
+                <p className="text-xl">{health_task.length} Tasks</p>
+              </div>
+              <div className="flex flex-col gap-4 bg-custom-white rounded-lg p-4">
+                {health_task.map((item, index) => (
+                  <TaskItem
+                    key={index}
+                    priority={item.priority}
+                    title={item.title}
+                    date_start={item.date_start}
+                    date_end={item.date_end}
+                    note={item.note}
+                    time_start={item.time_start}
+                    time_end={item.time_end}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer className="text-custom-white" />
+      </div>
+    </>
+  )
 }
 
 export default Health
