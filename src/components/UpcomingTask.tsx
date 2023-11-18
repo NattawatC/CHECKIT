44f2a514c593ba89a765e1fa1f2a65b184f639a2
NattawatC <64011478@kmitl.ac.th>
2023-11-18 20:45:03 +0700

    Refactor Link on upcoming task

--- src/components/UpcomingTask.tsx
+++ src/components/UpcomingTask.tsx
@@ -10,15 +10,15 @@ export const UpcomingTask: React.FunctionComponent<UpcomingTaskProps> = ({
   href,
 }) => {
   return (
-    <button className="w-full h-full bg-custom-white p-2 rounded-lg flex-col justify-center items-center gap-1 inline-flex">
-      <Link href={`${href}`}>
+    <Link href={`${href}`}>
+      <button className="w-full h-full bg-custom-white p-2 rounded-lg flex-col justify-center items-center gap-1 inline-flex">
         <div className="text-custom-black text-base font-medium break-words lg:text-2xl">
           Upcoming
         </div>
         <div className="text-custom-black text-base font-medium break-words lg:text-2xl">
           {taskNum} Tasks
         </div>
-      </Link>
-    </button>
+      </button>
+    </Link>
   )
 }
