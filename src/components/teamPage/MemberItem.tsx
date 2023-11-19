bc59a409ae862fd701fa6a6ad6c2d90579651f76
NattawatC <64011478@kmitl.ac.th>
2023-11-20 06:19:47 +0700

    Refactor MemberItem component

--- src/components/teamPage/MemberItem.tsx
+++ src/components/teamPage/MemberItem.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import {RxCross2} from 'react-icons/rx'
+// MemberItem.tsx
+import React from 'react';
+import { RxCross2 } from 'react-icons/rx';
 
 interface MemberItemProps {
-    email: string
+  email: string;
+  onRemove: () => void; // Callback function to remove the member
 }
 
-const MemberItem : React.FunctionComponent<MemberItemProps> = ({email}) => {
+const MemberItem: React.FunctionComponent<MemberItemProps> = ({ email, onRemove }) => {
   return (
-    <div className='flex flex-row gap-2 px-3 py-2 bg-custom-gray rounded-2xl w-fit items-center '>
-        <label className='text-custom-white'>
-            {email}
-        </label>
-        <RxCross2 className='text-custom-white' size='20' />
+    <div className="flex flex-row gap-2 px-3 py-2 bg-custom-gray rounded-2xl w-fit items-center">
+      <label className="text-custom-white">{email}</label>
+      <button className="text-custom-white hover:text-custom-orange" onClick={onRemove}>
+        <RxCross2 className="text-custom-white" size="20" />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default MemberItem
+export default MemberItem;
