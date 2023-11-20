ca989a5a97a3c35bed2e5a4828e55fad77b8e7bf
NattawatC <64011478@kmitl.ac.th>
2023-11-20 19:49:00 +0700

    Add search functionality to SearchBar component

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -4,17 +4,36 @@ import { Input } from '@/components/ui/input'
 import { useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { IoFilter } from 'react-icons/io5'
+import { searchTask } from '@/services/userServices'
+import { useEmail } from '@/components/EmailContext'
 
 interface SearchBarProps {
-  onCheckboxToggle: (itemName: string) => void;
+  onCheckboxToggle: (itemName: string) => void
+  onSearch: (searchQuery: string) => void
+  updateTasks: (tasks: Task[]) => void
 }
 
-export const SearchBar:  React.FC<SearchBarProps> = ({ onCheckboxToggle }) => {
+export const SearchBar:  React.FC<SearchBarProps> = ({ onCheckboxToggle, onSearch, updateTasks }) => {
+  const {email} = useEmail()
   const [isFilter, setIsFilter] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
 
   const toggleFilter = () => {
     setIsFilter(!isFilter)
   }
+
+  const handleSearch = async (query: string) => {
+    setSearchQuery(query)
+    onSearch(query)
+    try {
+      const results = await searchTask(query, email);
+      // Instead of setting search results state, update tasks directly
+      updateTasks(results);
+    } catch (error) {
+      console.error('Error searching task:', error);
+    }
+  };
+
   
   return (
     <div className="flex flex-col gap-4">
@@ -24,6 +43,7 @@ export const SearchBar:  React.FC<SearchBarProps> = ({ onCheckboxToggle }) => {
           type="text"
           placeholder="Search"
           className="w-full pl-12 pr-12 rounded-lg bg-custom-gray text-custom-white border-custom-white focus-visible:ring-custom-orange focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none lg:text-lg"
+          onChange={(e) => handleSearch(e.target.value)}
         />
         <Button
           className="absolute top-0 bottom-0 my-auto bg-transparent right-0 focus:bg-transparent"
